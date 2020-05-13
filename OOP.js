a9b11c6e64dca93fc3822ad248ffd48701d9a27e
@nbhankes <59675806+nbhankes@users.noreply.github.com>
2020-05-12 22:31:57 -0700

    changed two method to prototypes

--- OOP.js
+++ OOP.js
@@ -19,12 +19,7 @@ class cFactor {
     this.denomExp = denomExp;
     this.denomUnit = denomUnit;
     this.denomComp = denomComp;
-    this.getNum = function () {
-      return `${this.num} E ${this.numExp} ${this.numUnit} ${this.NumComp}`;
-    };
-    this.getDenom = function () {
-      return `${this.denom} E ${this.denomExp} ${this.denomUnit} ${this.denomComp}`;
-    };
+
     this.reverse = function () {
       this.denom = num;
       this.denomExp = numExp;
@@ -38,6 +33,14 @@ class cFactor {
     };
   }
 }
+
+cFactor.prototype.getNum = function () {
+  return `Numerator: ${this.num} E ${this.numExp} ${this.numUnit} ${this.NumComp}`;
+};
+
+cFactor.prototype.getDenom = function () {
+  return `Denominator: ${this.denom} E ${this.denomExp} ${this.denomUnit} ${this.denomComp}`;
+};
 
 gramsNitrogenPerMol = new cFactor(
   "Nitrogen grams per mol",
