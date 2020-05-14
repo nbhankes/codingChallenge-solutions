e1888c8edd33db9f4e60d9792dfe7d17e5f6ab9c
@nbhankes <59675806+nbhankes@users.noreply.github.com>
2020-05-13 19:50:28 -0700

    Create circleArea.js

---
+++ circleArea.js
@@ -0,0 +1,8 @@
+function main() {
+    const PI = Math.PI;
+    let r = 5;
+    // Print the area of the circle:
+   console.log(PI * r * r);
+    // Print the perimeter of the circle:
+   console.log(2 * PI * r);
+}
